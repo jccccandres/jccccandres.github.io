db4b5fcc65b5cbbdf858da97aeb807efe0ece083
jcandres <jc.andres17@gmail.com>
2020-05-09 23:24:51 +0800

    Add slider in header

--- js/general.js
+++ js/general.js
@@ -2,4 +2,32 @@ $(function(){
 	$(document).scroll(function() {
 		$(".navbar").toggleClass('change-bg', $(this).scrollTop() > $("header").height());
 	});
+
+
+
+
+	const sliders = $(".sliders");
+	let slider_index = 1;
+	showSlider(slider_index);
+
+	$(".prev-slide").on("click", () => {
+		slider_index = slider_index - 1;
+		if ( slider_index <= 0) {
+			slider_index = sliders.length;
+		}
+		showSlider(slider_index);
+	});
+
+	$(".next-slide").on("click", () => {
+		slider_index = slider_index + 1;
+		if ( slider_index > sliders.length) {
+			slider_index = 1;
+		}
+		showSlider(slider_index);
+	});
+
+	function showSlider(index) {
+		$(".sliders").removeClass("show");
+		$(".sliders:nth-child(" + index + ")").addClass("show");
+	}
 });
