c47e7a6a68671ae32133fd45d7bfcaa5432820c8
jcandres <jc.andres17@gmail.com>
2021-03-20 20:45:08 +0800

    Update style of scoreboard app page

--- js/react-scoreboard.js
+++ js/react-scoreboard.js
@@ -1,14 +1,37 @@
-function Header() {
+const Header = () => {	
 	return (
-		<header>
+		<div className="header">
 			<h1>Scoreboard</h1>
-			<span className="status">Players: 1</span>
-		</header>
+			<span className="stats">Players: 1</span>
+		</div>
 	);
 }
-console.log("Test");
+
+const Player = () => {
+	return (
+		<div className="player">
+			<span className="player-name">JC</span>
+			<div className="counter">
+				<button className="counter-action decrement"> - </button>
+				<span className="counter-score">35</span>
+				<button className="counter-action increment"> + </button>
+			</div>
+		</div>
+	);
+}
+
+const App = () => {
+	return (
+		<div className="scoreboard">
+			<Header />
+
+			<Player />
+		</div>
+	);
+}
+
 
 ReactDOM.render(
-	<Header />,
+	<App />,
 	document.getElementById('main_content')
 );
