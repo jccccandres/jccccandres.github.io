7c200580d056c5688375b7255e813e25e011a726
jcandres <jc.andres17@gmail.com>
2021-03-20 20:44:28 +0800

    Revert "Update style of scoreboard page"
    This reverts commit df146e6020e2dd5fd9cdb4d34fdb80d9d60dd19a.

--- js/react-scoreboard.js
+++ js/react-scoreboard.js
@@ -1,27 +1,14 @@
-const Header = () => {	
+function Header() {
 	return (
 		<header>
 			<h1>Scoreboard</h1>
-			<span className="stats">Players: 1</span>
+			<span className="status">Players: 1</span>
 		</header>
 	);
 }
-
-const Player = () => {
-	return (
-		<div className="player">
-			<span className="player-name">JC</span>
-			<div className="counter">
-				<button className="counter-action decrement"> - </button>
-				<span className="counter-score">35</span>
-				<button className="counter-action increment"> + </button>
-			</div>
-		</div>
-	);
-}
-
+console.log("Test");
 
 ReactDOM.render(
-	<Player />,
+	<Header />,
 	document.getElementById('main_content')
 );
